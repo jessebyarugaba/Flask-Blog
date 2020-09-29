1db12c04565286a5c2d86420bf0593e1aa07021e
Byarugaba Jesse <37853974+phino-phite@users.noreply.github.com>
2020-09-28 22:07:42 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id':1,
+			'title':'Article One',
+			'body':'This is the first article guys!!!!',
+			'author':'Byarugaba',
+			'create_date':'04-12-2017'
+		},
+		{
+			'id':2,
+			'title':'Article Two',
+			'body':'This is the first article guys!!!!',
+			'author':'Levix',
+			'create_date':'04-12-2017'
+		},
+		{
+			'id':3,
+			'title':'Article Three',
+			'body':'This is the first article guys!!!!',
+			'author':'John Doe',
+			'create_date':'04-12-2017'
+		}
+	]
+	
+	return articles
